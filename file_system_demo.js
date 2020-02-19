237ae6d61af899f724f300ed6b764d1e40d9ba6b
Madhura <madhura.bhujbal@gmail.com>
2020-02-19 16:09:33 +0000

    Create and write in folder

--- file_system_demo.js
+++ file_system_demo.js
@@ -1,8 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-//creating folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err=> {
+// //creating folder
+// fs.mkdir(path.join(__dirname, '/test'), {}, err=> {
+//     if (err) throw err;
+//     console.log('Folder created');
+// });
+
+//create and write in file
+fs.writeFile(path.join(__dirname, 'test', 'hello.txt' ),
+'Hello World!!',
+err=>{
     if (err) throw err;
-    console.log('Folder created');
+    console.log('Content written in file');
 });
+
+//write in file without creating folder
+fs.writeFile('hello.txt',
+'Hello World!!',
+err=>{
+    if (err) throw err;
+    console.log('Content written in file');
+});
+
+
