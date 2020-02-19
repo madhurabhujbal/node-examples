06fa7f56bd2dc2dff7d4fa268e14116ef065737b
Madhura <madhura.bhujbal@gmail.com>
2020-02-19 23:02:28 +0000

    Check host/root domain and hostname

--- url_module.js
+++ url_module.js
@@ -1,7 +1,13 @@
 const url = require('url');
 
-const myURL = new URL('https://www.google.com/');
+const myURL = new URL('https://www.google.com:8080/');
 
 //getting serialized url
 console.log(myURL.href);
 console.log(myURL.toString());
+
+//host or root domain, gets port number as well
+console.log(myURL.host);
+
+//hostname, returns everything same as host except the port number
+console.log(myURL.hostname);
