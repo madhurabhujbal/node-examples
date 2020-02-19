d84e943249ea97757583e209049f31ad7237caea
Madhura <madhura.bhujbal@gmail.com>
2020-02-19 22:41:55 +0000

    Add cpu info and check cpu memory details

--- file_system_demo.js
+++ file_system_demo.js
@@ -2,10 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // //creating folder
-// fs.mkdir(path.join(__dirname, '/test'), {}, err=> {
-//     if (err) throw err;
-//     console.log('Folder created');
-// });
+
 
 // //create and write in folder
 fs.writeFile(path.join(__dirname, 'test', 'hello.txt' ),
--- os_module.js
+++ os_module.js
@@ -1,3 +1,13 @@
 const os = require('os');
 
+//OS platform and architecture
 console.log('Platform:' + os.platform + '\n' + 'Architecture:' + os.arch);
+
+//OS cpu info
+console.log(os.cpus());
+
+//Total computer memory
+console.log('Total memory: '+os.totalmem());
+
+//Free computer memory
+console.log('Free memory: '+os.freemem());
